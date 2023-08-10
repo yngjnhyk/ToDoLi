3584e8f8d5f1ae51ada1a0feb23b4d08b5056b46
jh <dskzpbmk@naver.com>
2023-08-10 13:18:04 +0900

    first commit

--- app.py
+++ app.py
@@ -21,49 +21,85 @@ def loginPage():
 # 몽고DB에 닉네임, 이메일, 비밀번호 데이터 넣기
 @app.route("/login", methods=["POST"])
 def login():
-    # userInt = 0
+    userInt = 0
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     nick_receive = request.form['nick_give']
-    # todoMemo_receive = request.form['todoMemo_give']
-    # done_receive = request.form['done_give']
-    # date_receive = request.form['date_give']
+    
     
     doc = {
-        # 'userInt': userInt,
+        'nickname' : nick_receive,
         'id': id_receive,
         'pw': pw_receive,
-        'nickname' : nick_receive,
-        # 'todoList': [{'todoMemo' : todoMemo_receive,
-        #                 'done' : done_receive,
-        #                 'date' : date_receive}],
         
     }
-    # userInt += 1 
 
     # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.todo.find_one({'id': id_receive, 'pw': pw_receive, 'nickname': nick_receive})
+    result = db.user.find_one({'id': id_receive, 'pw': pw_receive, 'nickname': nick_receive})
 
     # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
 
         # token을 줍니다.
-        return jsonify({'result': 'success', 'userNickname': result['nickname']})
+        return jsonify({'result': 'success', 'userInt': result['userInt']})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+# 몽고DB에 to-do-list 데이터 넣기
+@app.route("/todo", methods=["POST"])
+def todo_post():
+    date_receive = request.form['date_give']
+    list_receive = request.form['list_give']
+
+    doc = {
+        'date': date_receive,
+        'list': list_receive,
+        'done' : 0
+    }
+    db.todo.insert_one(doc)
+    return jsonify({'msg': '입력 완료!'})
+
+
+#투두 리스트 완료 체크할 때
+@app.route("/todo/done", methods=["POST"])
+def todo_done():
+    id_receive = request.form['id_give']
+    db.todo.update_one({'_id': ObjectId(id_receive)}, {'$set': {'done': 1}})
+
+    return jsonify({'msg': '완료!'})
+
+#투두 완료 취소 
+@app.route("/todo/cancel", methods=["POST"])
+def todo_cancel():
+    id_receive = request.form['id_give']
+    db.todo.update_one({'_id': ObjectId(id_receive)}, {'$set': {'done': 0}})
+    return jsonify({'msg': '완료 취소!'})
+
+#투두 삭제
+@app.route("/todo/delete", methods=["POST"])
+def todo_delete():
+    id_receive = request.form['id_give']
+    db.todo.delete_one({'_id': ObjectId(id_receive)})
+    return jsonify({'msg': '삭제 완료'})
+
 
 #userInt 가져오기
 @app.route("/userInt", methods=["GET"])
 def userInt():
-    userNickname_receive = request.cookies.get('userNickname')
-    userinfo = db.todo.find_one({'nickname': userNickname_receive}, {'_id': 0})
-
+    userInt_receive = int(request.cookies.get('userInt'))
+    userinfo = db.user.find_one({'userInt': userInt_receive}, {'_id': 0})
     
-    return jsonify({'result': userinfo, 'userNickname':userNickname_receive})
+    return jsonify({'result': userinfo, 'userInt':userInt_receive})
 
+#몽고디비에서 num 값 대신 ID 값 가져오기
+@app.route("/todo", methods=["GET"])
+def todo_get():
+    userInt_receive = int(request.cookies.get('userInt'))
+    todoinfo = db.todo.find_one({'userInt': userInt_receive}, {'_id': 0})
+    
+    return jsonify({'result': todoinfo, 'userInt':userInt_receive})
 
 
 
